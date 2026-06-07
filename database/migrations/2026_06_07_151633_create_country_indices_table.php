4278a902698f36a751b8715b62d0475fc0705603
Marks Kalijevs <markskalijevs@gmail.com>
2026-06-07 20:53:34 +0300

    refactor: rename country_indexes table to country_indices

--- database/migrations/2026_06_07_151633_create_country_indices_table.php
+++ database/migrations/2026_06_07_151633_create_country_indices_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('country_indexes', function (Blueprint $table) {
+        Schema::create('country_indices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('country_id')->constrained()->onDelete('cascade');
-            $table->decimal('gini', 4, 3)->nullable();
+            $table->decimal('gini', 5, 2)->nullable();
             $table->unsignedSmallInteger('gini_year')->nullable();
             $table->decimal('hdi', 4, 3)->nullable();
             $table->timestamps();
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('country_indexes');
+        Schema::dropIfExists('country_indices');
     }
 };
